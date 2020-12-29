dd7a13291f0f43b4644bdff38c546d5713a79eff
zbert <zbert@hotmail.com>
2020-12-28 18:11:31 -0600

    Added favicon and fontawesome.min.css

--- js/main.js
+++ js/main.js
@@ -13,13 +13,46 @@ const updateStatus = (txt) => {
   stat.innerHTML = txt;
 };
 
+// Get Pixel Color
+const getColorXY = (x, y, width) => {
+  const base = y * (width * 4) + x * 4;
+  return [base, base + 1, base + 2, base + 3];
+};
+
 // Function to Copy Image As Is
 const copyImage = () => {
+  // TODO: Globalize context
+  const image = document.getElementById("image");
+  const c = document.getElementById("canvas");
+  const ctx = c.getContext("2d");
+
+  ctx.drawImage(image, 0, 0);
+
   updateStatus("Copy");
 };
 
 // Function to Convert to Greyscale
 const toBW = () => {
+  // TODO: Globalize Context
+  const image = document.getElementById("image");
+  const c = document.getElementById("canvas");
+  const ctx = c.getContext("2d");
+
+  let x = 0;
+  let y = 0;
+  let width = image.width;
+  let height = image.height;
+
+  // Copy Image
+  ctx.drawImage(image, 0, 0);
+  // Get Image Data
+  let pixel = ctx.getImageData(0, 0, image.width, image.height);
+  let data = pixel.data;
+  console.log(data);
+  // Alter Each Pixel
+  // Display Result
+  // Update Status
+
   updateStatus("Grey");
 };
 
